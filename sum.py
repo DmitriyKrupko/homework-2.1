c7c47eac78ee03f12a27ede78b751da02bac45c8
Makoto <dimonkrupko@yandex.ru>
2024-11-11 20:42:12 +0300

    sum

--- sum.py
+++ sum.py
@@ -1,5 +1,5 @@
 print("Введите сумму заказа")
-a = float(input())
+a = int(input())
 waiter = a * 0.18
 tax = a * 0.04
 print("Сумма вашего заказа: ", a,"\n" "Чаевые официанту: ", waiter,"\n" "Налог: ", tax, sep=" ")
